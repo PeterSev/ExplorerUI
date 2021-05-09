fa88f4fabbef895df7fd7e61d1620e3b06553149
PeterSev <petersevos@mail.ru>
2021-05-10 00:33:54 +0300

    InitialCommit

---
+++ Core/Explorer.Shared.ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Explorer.Shared.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        #region Public Properties
+        public string MainDiskName { get; set; }
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Contructor
+        public MainViewModel()
+        {
+            MainDiskName = Environment.SystemDirectory;
+        }
+        #endregion
+
+        #region Protected Methods
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
---
+++ WPF/Explorer.WPF.UI/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Explorer.Shared.ViewModels;
+using System.Windows;
+
+namespace Explorer.WPF.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
