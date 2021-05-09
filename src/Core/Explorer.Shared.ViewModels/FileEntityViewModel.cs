58f7d6cf591d9116a80cb4629b9f7cf82c6bff17
PeterSev <petersevos@mail.ru>
2021-05-10 01:35:35 +0300

    DirectoriesAndFiles is added. MouseDoubleClick event is handled to invoke 'OpenCommand' command

---
+++ src/Core/Explorer.Shared.ViewModels/FileEntityViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Explorer.Shared.ViewModels
+{
+    public abstract class FileEntityViewModel : BaseViewModel
+    {
+        public string FullName { get; set; }
+        public string Name { get; }
+        protected FileEntityViewModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- src/Core/Explorer.Shared.ViewModels/MainViewModel.cs
+++ src/Core/Explorer.Shared.ViewModels/MainViewModel.cs
@@ -1,32 +1,53 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace Explorer.Shared.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : BaseViewModel
     {
-
-
         #region Public Properties
-        public string MainDiskName { get; set; }
+        public ObservableCollection<FileEntityViewModel> DirectoriesAndFiles { get; set; } = new ObservableCollection<FileEntityViewModel>();
+        public FileEntityViewModel SelectedFileEntity { get; set; }
+        public string FilePath { get; set; }
         #endregion
 
-        #region Events
-        public event PropertyChangedEventHandler PropertyChanged;
+        #region Commands
+        public ICommand OpenCommand { get; }
+        #endregion
+
+        #region CommandsMethods
+        private void Open(object parameter)
+        {
+            if(parameter is DirectoryViewModel dirViewModel)
+            {
+                FilePath = dirViewModel.FullName;
+                DirectoriesAndFiles.Clear();
+
+                var dirInfo = new DirectoryInfo(FilePath);
+                foreach(var dir in dirInfo.GetDirectories())
+                {
+                    DirectoriesAndFiles.Add(new DirectoryViewModel(dir));
+                }
+                foreach (var fileInfo in dirInfo.GetFiles())
+                {
+                    DirectoriesAndFiles.Add(new FileViewModel(fileInfo));
+                }
+
+            }
+        }
         #endregion
 
         #region Contructor
         public MainViewModel()
         {
-            MainDiskName = Environment.SystemDirectory;
-        }
-        #endregion
-
-        #region Protected Methods
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            OpenCommand = new DelegateCommand(Open);
+            foreach(var logicalDrive in Directory.GetLogicalDrives())
+            {
+                DirectoriesAndFiles.Add(new DirectoryViewModel(logicalDrive));
+            }
         }
         #endregion
 
